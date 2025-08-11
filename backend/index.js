d5cdebbc58f085a0ca420bb8aa3d764762295068
Rawlinrepo <rawlinleitao08@gmail.com>
2025-08-11 21:13:12 +0530

    Update index.js

--- backend/index.js
+++ backend/index.js
@@ -6,7 +6,7 @@ const app = express();
 require('dotenv').config();
 const PORT = process.env.PORT || 5000;
 
-app.use(cors());
+app.use(cors({ origin: "*" }));
 app.use(express.json());
 
 // API route
